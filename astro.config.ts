3f5499af729b6341b48342ceedd0f256dcab498d
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2025-07-25 21:58:23 +0200

    Remove unneeded (turns out) image integration.

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import image from "@astrojs/image";
-
 export default defineConfig({
   base: "/",
   outDir: "dist",
-  integrations: [image()],
 });

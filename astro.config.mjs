cc5e0e796c2601452a4ee51378024f52507b8096
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2025-07-25 20:31:29 +0200

    Update astro config, adapted for gh-pages deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    base: "/",
+    outDir: "dist",
+
+});

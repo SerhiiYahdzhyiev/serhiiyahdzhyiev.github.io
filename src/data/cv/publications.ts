ee18097aa0e9180de354abde007dd7c79dab5452
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2026-03-22 11:26:37 +0100

    Update data and cv page.

---
+++ src/data/cv/publications.ts
@@ -0,0 +1,22 @@
+export const publications = [
+  {
+    year: 2025,
+    title: "On the Usability and Energy Efficiency of High-Level Synthesis for FPGA-based Network-Attached Accelerators",
+    authors: [
+      "Steffen Christgau",
+      "Dylan Everingham",
+      "Marco De Lucia",
+      "Max Lubke",
+      "Danny Puhan",
+      "Niklas Schelten",
+      "Bettina Schnor",
+      "Hannes Signer",
+      "Johannes Spazier",
+      "Benno Stabernack",
+      "Fritjof Steinert",
+      "Serhii Yahdzhyiev",
+    ],
+    venue: undefined as string | undefined,
+    url: undefined as string | undefined,
+  },
+];

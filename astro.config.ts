d2a16c50a597e0198eb2ac7577fd6ec751965152
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2026-03-22 15:00:26 +0100

    Improve perfomance and seo a bit.

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
-import compressor from "astro-compressor";
+import compress from "@playform/compress";
 import UnoCSS from "unocss/astro";
 import react from "@astrojs/react";
 
@@ -18,7 +18,26 @@ export default defineConfig({
       lastmod: new Date(),
       filter: (page) => !page.includes("/blog/"),
     }),
-    compressor(),
     UnoCSS(),
+    compress({
+      CSS: true,
+      HTML: true,
+      JavaScript: true,
+      SVG: true,
+      Image: false,
+    }),
   ],
+  vite: {
+    build: {
+      target: "es2020",
+      cssMinify: true,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules/react")) return "react-vendor";
+          },
+        },
+      },
+    },
+  },
 });

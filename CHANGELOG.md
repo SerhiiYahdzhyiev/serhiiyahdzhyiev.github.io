523b34ebf496309ec167ecc49906f53e610d98df
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2026-04-05 10:42:11 +0200

    Add recent publication.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,18 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/).
 
+## 0.1.1
+
+### Added
+
+- New cover videos
+- Recent publication from NAAICE
+
+### Changed
+
+- Bio text
+- Skills data
+
 ## 0.1.0
 
 ### Added
--- src/data/cv/publications.ts
+++ src/data/cv/publications.ts
@@ -1,5 +1,24 @@
 export const publications = [
   {
+    year: 2026,
+    title:
+      "Using FPGA-based Network-Attached Accelerators for Energy-Efficient AI Training in HPC Datacenters",
+    authors: [
+      "Niklas Schelten",
+      "Steffen Christgau",
+      "Merit Hutzler",
+      "Philipp Kreowsky",
+      "Marco de Lucia",
+      "Bettina Schnor",
+      "Hannes Signer",
+      "Johannes Spazier",
+      "Benno Stabernack",
+      "Serhii Yahdzhyiev",
+    ],
+    venue: "33rd Reconfigurable Architectures Workshop (RAW 2026), IPDPS, New Orleans, USA",
+    url: undefined as string | undefined,
+  },
+  {
     year: 2025,
     title:
       "On the Usability and Energy Efficiency of High-Level Synthesis for FPGA-based Network-Attached Accelerators",
